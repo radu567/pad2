d61183d8ebfe933e4fbabfe1f668546f76acf825
radu567 <filipesco@mail.ru>
2016-11-16 17:39:07 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,26 +3,34 @@ import struct
 import sys
 import json
 
+
 class Group:
-    def run(self, ip_multicast, port_multicast, ip_tcp, port_tcp, data, relation):
-        # self.adr = (ip_multicast, port_multicast, relation)
+    def __init__(self, ip_multicast, port_multicast, ip_tcp, port_tcp, data, relation):
+        self.ip_multicast = ip_multicast
+        self.port_multicast = port_multicast
+        self.ip_tcp = ip_tcp
+        self.port_tcp = port_tcp
+        self.data = data
+        self.relation = relation
+
+    def run(self):
 
         # Facem socketul
-        sock_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock_UDP.bind((ip_multicast, port_multicast))
+        sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock_udp.bind((self.ip_multicast, self.port_multicast))
 
         # Adaugam sistemul la grupul multicast in toate interfetele
-        group = socket.inet_aton(ip_multicast)
+        group = socket.inet_aton(self.ip_multicast)
         mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-        sock_UDP.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        sock_udp.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-        relations = len(relation)
+        relations = len(self.relation)
 
         # Receive/respond loop (bulca)
 
         while True:
             print("Astepare conexiuni")
-            data, address = sock_UDP.recvfrom(1024)
+            data, address = sock_udp.recvfrom(1024)
             # sys.stderr - Fluxul standart de erori.
             print(sys.stderr, 'primiti %s bytes mesaj de la %s' % (len(data), address))
             print(sys.stderr, data)
@@ -32,23 +40,23 @@ class Group:
             # se creaza obiectul json cu datele si se trimite
             data = {
                 'relations': relations,
-                'ip_tcp': ip_tcp,
-                'port_tcp': port_tcp
+                'ip_tcp': self.ip_tcp,
+                'port_tcp': self.port_tcp
             }
-            jsonObj = json.dumps(data).encode('utf-8')
-            sock_UDP.sendto(jsonObj)
+            json_obj = json.dumps(data).encode('utf-8')
+            sock_udp.sendto(json_obj)
 
         # creare socket TCP
-        sock_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock_TCP.connect(ip_tcp, port_tcp)
+        sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_tcp.connect(self.ip_tcp, self.port_tcp)
 
         while True:
-            clientsocket, addr = sock_TCP.accept()
-            info = sock_TCP.recv(1024)
+            clientsocket, addr = sock_tcp.accept()
+            info = sock_tcp.recv(1024)
             if info:
                 print(info)
             else:
                 clientsocket.close()
 
-node1 = Group
-node1.run('224.3.29.71', 10000, '127.0.0.1', '9991', 'node1', [('127.0.0.2', '9992'), ('127.0.0.3', '9993'), ('127.0.0.4', '9994')])
+node1 = Group('224.3.0.64', 10000, '127.0.0.1', '9991', 'node1', [('127.0.0.1', '9992'), ('127.0.0.3', '9993')])
+node1.run()
