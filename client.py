6d24ebbeb44f581d8759bdeedeccc58fccfda262
radu567 <filipesco@mail.ru>
2016-11-19 18:52:32 +0200

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import sys
 import json
 
 message = 'Clientul cere informatie'
-message = str.encode(message)
+message = message.encode('utf-8')
 multicast_group = ('224.3.29.71', 10000)
 
 # Se creaza datagram socket
@@ -23,42 +23,48 @@ rel = []
 ip = []
 port = []
 
+# Trimite date la grupul multicast
+print(sys.stderr, 'sending "%s"' % message)
+sent = sock.sendto(message, multicast_group)
+
+# Cautare raspunsuri de la destinatari
 while i < 6:
+    print(sys.stderr, 'waiting to receive')
+    try:
+        # se primeste segmentul de date si se citeste din json
+        data, address = sock.recvfrom(2048)
+        data = json.loads(data.decode('utf-8'))
+        relations = data.get('relations')
+        ip_tcp = data.get('ip_tcp')
+        port_tcp = data.get('port_tcp')
 
-    # Trimite date la grupul multicast
-    print(sys.stderr, 'sending "%s"' % message)
-    sent = sock.sendto(message, multicast_group)
-
-    # Cautare raspunsuri de la destinatari
-    while i < 6:
-        print(sys.stderr, 'waiting to receive')
-        try:
-            # se primeste segmentul de date si se citeste din json
-            data = sock.recvfrom(2048)
-            data = json.loads(data.decode('utf-8'))
-            relations = data.get('relations')
-            ip_tcp = data.get('ip_tcp')
-            port_tcp = data.get('port_tcp')
-
-        except socket.timeout:
-            print(sys.stderr, 'timed out, no more responses')
-            break
-        else:
-            print(sys.stderr, '1 nod are %s legaturi, ip-ul: %s si portul: %s' % (relations, ip_tcp, port_tcp))
-            rel.append(relations)
-            ip.append(ip_tcp)
-            port.append(port_tcp)
+    except socket.timeout:
+        print(sys.stderr, 'timed out, no more responses')
+        break
+    else:
+        print(sys.stderr, '1 nod are %s legaturi, ip-ul: %s si portul: %s' % (relations, ip_tcp, port_tcp))
+        rel.append(relations)
+        ip.append(ip_tcp)
+        port.append(port_tcp)
 
 print(sys.stderr, 'closing socket')
 sock.close()
 
 # cautam nodul cu cele mai multe relatii
 pozitia = rel.index(max(rel))
+print(ip[pozitia])
 sock_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock_TCP.bind(ip[pozitia], port[pozitia])
 sock_TCP.listen(5)
 
 while True:
+# mesajul de cerere
+    cerere = {
+        'type': 'client',
+        'message': ''
+        }
+    jsonobj = json.dumps(cerere).encode('utf-8')
+    sock_TCP.send(jsonobj)
 
     datas = sock_TCP.recv(1024)
     datas = datas.decode('utf-8')
