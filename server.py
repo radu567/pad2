d070b71dd6dc38f066b9b1998c42d98a431be6b3
radu567 <filipesco@mail.ru>
2016-11-20 13:57:10 +0200

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import sys
 import json
 import collections
 import queue
+import threading
+from threading import Thread
 
 MESSAGE_TYPE = collections.namedtuple('MessageType', ('client', 'node'))(*('client', 'node'))
 lista_date = queue.Queue()
@@ -58,7 +60,8 @@ class Nod:
 
         # creare socket TCP ....... date utilizate pentru TCP
         sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock_tcp.connect(self.ip_tcp, self.port_tcp)
+        sock_tcp.bind((self.ip_tcp, self.port_tcp))
+        print('socket tcp in functiune')
 
         while True:
             clientsocket, addr = sock_tcp.accept()
@@ -69,23 +72,54 @@ class Nod:
             if type == MESSAGE_TYPE.node:
                 info = self.data.encode('utf-8')
                 sock_tcp.send(info)
+
             if type == MESSAGE_TYPE.client:
-                cerere = {
-                    'type': 'node',
-                    'message': ''
-                }
-                jsonobj = json.dumps(cerere).encode('utf-8')
-                sock_tcp.send(jsonobj)
+                # facem citirea de pe fiecare nod
+                i = 0
+                relatii = len(self.relation)
+                lista_date.put(self.data)
 
-                raspuns = sock_tcp.recv(1024)
-                raspuns = raspuns.decode('utf-8')
+                while i < relatii:
+                    # preluam prima relatie pentru a ne conecta la acest nod
+                    ip, port = self.relation[i]
+                    sock_node = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock_node.connect(ip, port)
+                    # facem cererea la nod
+                    cerere = {
+                        'type': 'node',
+                        'message': ''
+                    }
+                    jsonobj = json.dumps(cerere).encode('utf-8')
+                    sock_node.send(jsonobj)
 
-                lista_date.put(raspuns)
+                    # asteptam raspuns de la nod
+                    raspuns = sock_node.recv(1024)
+                    raspuns = raspuns.decode('utf-8')
+                    # adaugam raspunsul in lista de date
+                    lista_date.put(raspuns)
+                    sock_node.close()
+                    i += 1
 
+                # atit timp cit avem date in lista le trimitem clientului
+                if lista_date:
+                    m = lista_date.get()
+                    # print(m)   # comentat de radu
+                    sock_tcp.send(m.encode("utf-8"))
+
+                else:
+                    sock_tcp.send('Queue is empty')
 
             else:
                 clientsocket.close()
 
+# initiem nodurile
+node1 = Nod('224.3.29.71', ('', 10000), '127.0.0.1', 9991, 'node1', [('127.0.0.2', '9992'), ('127.0.0.6', '9996')])
+node2 = Nod('224.3.29.71', ('', 10000), '127.0.0.2', 9992, 'node2', [('127.0.0.1', '9991'), ('127.0.0.3', '9993'), ('127.0.0.6', '9996'), ('127.0.0.5', '9995')])
+node3 = Nod('224.3.29.71', ('', 10000), '127.0.0.3', 9993, 'node3', [('127.0.0.2', '9992')])
+node4 = Nod('224.3.29.71', ('', 10000), '127.0.0.4', 9994, 'node4', [])
+node5 = Nod('224.3.29.71', ('', 10000), '127.0.0.5', 9995, 'node5', [('127.0.0.2', '9992')])
+node6 = Nod('224.3.29.71', ('', 10000), '127.0.0.6', 9996, 'node6', [('127.0.0.1', '9991'), ('127.0.0.2', '9992')])
 
-node1 = Nod('224.3.29.71', ('', 10000), '127.0.0.1', '9991', 'node1', [('127.0.0.1', '9992'), ('127.0.0.3', '9993')])
-node1.listen_udp()
+if __name__ == '__main__':
+    Thread(target=node1.listen_tcp()).start()
+    Thread(target=node1.listen_udp()).start()
